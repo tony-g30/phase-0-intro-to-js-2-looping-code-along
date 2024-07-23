4b9d3cee53f0134f4cef74af447b5e18c885d16c
tony-g30 <tonyaudo@gmail.com>
2024-07-23 07:04:00 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+  
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+  countDown(10);
+  function writeCards(names) {
+    const messages = names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+    return messages;
+  }
+  
